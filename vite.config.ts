dcb28010c1f1142e0003ccbbb5b0c49cd99aaeb1
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-05-01 00:42:36 +0530

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/anubhav-auth.github.io/',
+  base: '/anubhav-auth.github.io',
   server: {
     host: "::",
     port: 8080,
